fd86eadb7d50ce94ffbba311e6a775bb1c7d61d2
Yousuf Hosen <hasanyousuf.ml.django@gmail.com>
2025-11-13 21:54:58 +0600

    env setup

--- food_online/settings.py
+++ food_online/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!!v&61e4glv5yxf5^xno$ribir0rzmgphqdht-_4#l_jf1@54b"
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
+
 
 ALLOWED_HOSTS = []
 
@@ -75,10 +78,10 @@ WSGI_APPLICATION = "food_online.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "foodbakery_db",
-        "USER": "postgres",
-        "PASSWORD": "dbadmin",
-        "HOST": "localhost"
+        "NAME": config('DB_NAME'),
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('HOST')
 
     }
 }
